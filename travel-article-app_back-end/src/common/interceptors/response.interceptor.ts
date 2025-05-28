cf5de9787d967ae3b8ce9263229cba66477aae20
ixhsan <muharipan@gmail.com>
2025-05-28 20:30:23 +0700

    feat(be-auth): added common filter, interceptors and pipes

---
+++ travel-article-app_back-end/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class ResponseTransformInterceptor implements NestInterceptor {
+  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        message: 'OK',
+        data,
+      })),
+    );
+  }
+}
